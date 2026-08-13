c431311786d5ceeb37a4b689a9f0c50af9f40567
Bolllix <o.soelva@gmail.com>
2026-08-13 13:38:47 +0200

    Beschreibung deiner Änderung (z.B. Neue Regeln hinzugefügt)

--- src/main/java/de/zuordner/ZuordnerApplication.java
+++ src/main/java/de/zuordner/ZuordnerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZuordnerApplication {
+public class ZuordnerApplication {                      // hier startet die App mit Springboot
 
     public static void main(String[] args) {
         SpringApplication.run(ZuordnerApplication.class, args);
